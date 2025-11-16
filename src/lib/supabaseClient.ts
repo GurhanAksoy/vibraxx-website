6834841a5ef80c8a850ee0222e4a15a69466c194
GurhanAksoy <aksoy75@gmail.com>
2025-11-16 08:20:54 +0300

    Update supabaseClient.ts

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,16 +1,12 @@
-import { createClient } from "@supabase/supabase-js";
+import { createBrowserClient } from "@supabase/ssr";
 
-const isBrowser = typeof window !== "undefined";
-
-export const supabase = createClient(
+export const supabase = createBrowserClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
   {
     auth: {
       persistSession: true,
       autoRefreshToken: true,
-      detectSessionInUrl: true,
-      storage: isBrowser ? localStorage : undefined,
     },
   }
 );
