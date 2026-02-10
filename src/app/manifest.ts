d8605e468d90613de50df4accb4c38ea815a6fca
GurhanAksoy <aksoy75@gmail.com>
2026-02-10 04:04:30 +0300

    Update manifest.ts

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -14,13 +14,13 @@ export default function manifest(): MetadataRoute.Manifest {
         src: "/icons/manifest-icon-192.maskable.png",
         sizes: "192x192",
         type: "image/png",
-        purpose: "any maskable",
+        purpose: "maskable", // ✅
       },
       {
         src: "/icons/manifest-icon-512.maskable.png",
         sizes: "512x512",
         type: "image/png",
-        purpose: "any maskable",
+        purpose: "any", // ✅
       },
     ],
   };
